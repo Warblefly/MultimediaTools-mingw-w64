4fd1e72e52c5659f13645b18590b34aeb790c01a
Warblefly <john@johnwarburton.net>
2015-10-01 19:59:30 +0000

    Change FLAC git URL to one that works

--- cross_compile_ffmpeg_shared.sh
+++ cross_compile_ffmpeg_shared.sh
@@ -1945,7 +1945,7 @@ build_ffms2() {
 }
 
 build_flac() {
-  do_git_checkout git://git.xiph.org/flac.git flac
+  do_git_checkout https://git.xiph.org/flac.git flac
   cd flac
   # microbench target hasn't been tested on many platforms yet
   sed -i.bak 's/microbench//' Makefile.am
