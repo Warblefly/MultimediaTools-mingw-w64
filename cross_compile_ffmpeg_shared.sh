83ab09a93cb46e5cf986e395621c07b8c7d35f26
John Warburton <john@johnwarburton.net>
2021-04-06 15:42:35 +0100

    Return to vanilla youtube-dl.

--- cross_compile_ffmpeg_shared.sh
+++ cross_compile_ffmpeg_shared.sh
@@ -5084,10 +5084,10 @@ build_libde265() {
 }
 
 build_youtube-dl() {
-  do_git_checkout https://github.com/blackjack4494/yt-dlc.git yt-dlc #https://github.com/rg3/youtube-dl
-  cd yt-dlc
-    do_make youtube-dlc
-    cp youtube-dlc "${mingw_w64_x86_64_prefix}/bin/youtube-dl.py"
+  do_git_checkout https://github.com/rg3/youtube-dl youtube-dl
+  cd youtube-dl
+    do_make youtube-dl
+    cp youtube-dl "${mingw_w64_x86_64_prefix}/bin/youtube-dl.py"
   cd ..
 }
 
@@ -6879,7 +6879,7 @@ build_ffmpeg() {
 	local component_options="--enable-filter=frei0r --enable-decoder=aac"
 	local library_options="--enable-avisynth --enable-chromaprint --enable-frei0r --enable-ladspa --enable-libaom --enable-libass --enable-libbluray --enable-libbs2b --enable-libcaca --enable-libcelt --enable-libcdio --enable-libcodec2 --enable-libdc1394 --enable-libfdk-aac --enable-libflite --enable-libfontconfig --enable-libfreetype --enable-libfribidi --enable-libgme --enable-gnutls --enable-libgsm --enable-libilbc --enable-libjack --enable-libklvanc --enable-liblensfun --enable-libmodplug --enable-libmp3lame --enable-libopencore-amrnb --enable-libopencore-amrwb --enable-libopencv --enable-libopenmpt --enable-libopus --enable-librabbitmq --enable-librist --enable-librubberband --enable-librtmp --enable-libsnappy --enable-libsoxr --enable-libspeex --enable-libsrt --enable-libtesseract --enable-libtheora --enable-libtwolame --enable-libvidstab --enable-libvmaf --enable-libvo-amrwbenc --enable-libvorbis --enable-libvpx --enable-libwebp --enable-libx264 --enable-libx265 --enable-libxavs --enable-libxavs2 --enable-libxvid --enable-libxml2 --enable-libzimg --enable-libzmq --enable-libzvbi --enable-lv2 --enable-decklink --enable-libmysofa --enable-opencl --enable-opengl --enable-vulkan"
 	local hardware_options="--enable-cuda-nvcc --enable-libmfx"
-	local toolchain_options="--arch=x86_64 --cross-prefix=$cross_prefix --enable-cross-compile --target-os=mingw32 --extra-version=Compiled_by_John_Warburton --enable-pic --extra-cflags=-I${mingw_w64_x86_64_prefix}/include/fribidi/ --extra-cflags=-I${mingw_w64_x86_64_prefix}/include/lensfun/"
+	local toolchain_options="--arch=x86_64 --cross-prefix=$cross_prefix --enable-cross-compile --target-os=mingw32 --extra-version=Compiled_by_John_Warburton --enable-pic --extra-cflags=-I${mingw_w64_x86_64_prefix}/include/fribidi/ --extra-cflags=-I${mingw_w64_x86_64_prefix}/include/lensfun/ --extra-cflags=-I${mingw_w64_x86_64_prefix}/include/opus/"
 	local developer_options="--disable-debug --enable-stripping"
 	cd ffmpeg_git
 
