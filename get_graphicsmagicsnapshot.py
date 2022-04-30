daa3b6306e03a30c7a43b71c0d99133af47cdc99
Warblefly <john@johnwarburton.net>
2022-04-30 10:10:52 +0100

    Program to retrieve latest GraphicsMagick

---
+++ get_graphicsmagicsnapshot.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+
+from bs4 import BeautifulSoup
+import requests
+import re
+
+url = 'https://sourceforge.net/projects/graphicsmagick/files/graphicsmagick-snapshots/'
+
+html_text = requests.get(url).text
+soup = BeautifulSoup(html_text, "html.parser")
+filename = soup.find_all(string=re.compile("GraphicsMagick-1.4."))[0]
+directory = filename[:-7]
+
+print(url + filename + ";" + directory)
+
+
